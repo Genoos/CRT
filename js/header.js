3b456676d5afd833b8816fe4e49dbd5e03f0d901
sandeep-db <sandeep.b@darwinbox.com>
2023-03-15 11:49:45 +0530

    Fixed Gallery

--- js/header.js
+++ js/header.js
@@ -5,6 +5,15 @@ const username = document.querySelector('#user-name')
 const nusername = document.querySelector('#n-user-name')
 
 function onLoad() {
+    
+    var new_loc = localStorage.getItem('location')
+    let user_loc = document.getElementById("user_location")
+    if (new_loc == null) {
+        user_loc.innerText = "Select Location"
+    } else {
+        user_loc.innerText = new_loc
+    }
+
     let name = localStorage.getItem("username")
     if (!name) {
         return
@@ -12,7 +21,9 @@ function onLoad() {
     username.innerText = name
     nusername.innerText = name
     login_logout.innerText = "Logout"
+
 }
+
 
 window.onload = onLoad
 window.onblur = onLoad
--- js/login.js
+++ js/login.js
@@ -1,13 +1,3 @@
-var new_loc=localStorage.getItem('location')
-let user_loc=document.getElementById("user_location")
-if(new_loc==null){
-    
-    user_loc.innerText="Select Location"
-}
-else{
-    user_loc.innerText=new_loc
-}
-
 
 
 function fetchData(data) {
