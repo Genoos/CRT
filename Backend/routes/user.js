333f7d762fef2563be84361f108b125d58b0188c
sandeep-db <sandeep.b@darwinbox.com>
2023-03-29 13:03:14 +0530

    profile change

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,73 +1,72 @@
-import express from "express";
-import UserController from "../controllers/user.js";
-import upload from "../modules/fileHandler.js";
+import express from "express"
+import UserController from "../controllers/user.js"
+import upload from "../modules/fileHandler.js"
 
-const app = express.Router();
-app.use(express.json());
-const userController = UserController();
+const app = express.Router()
+app.use(express.json())
+const userController = UserController()
 
 app.route("/login").post(async (req, res) => {
-  let data = await userController.authenticate(req.body);
-
-  if (data) {
-    res.status(200).json(data);
-  } else {
-    res.status(200).json({ errno: 404, message: "Data not found" });
-  }
-});
+	let data = await userController.authenticate(req.body)
+	if (data) {
+		res.status(200).json(data)
+	} else {
+		res.status(200).json({ errno: 404, message: "Data not found" })
+	}
+})
 
 app.route("/register").post(async (req, res) => {
-  let data = await userController.createUser(req.body);
+	let data = await userController.createUser(req.body)
 
-  if (data["email"]) {
-    res.status(200).json(data);
-  } else {
-    res.status(200).json({ errno: 406, message: "Error in inserting" });
-  }
-});
+	if (data["email"]) {
+		res.status(200).json(data)
+	} else {
+		res.status(200).json({ errno: 406, message: "Error in inserting" })
+	}
+})
 
 app.route("/cars").post(async (req, res) => {
-  console.log("Y");
-  console.log(req.body);
-  let data = await userController.getCars(req.body);
-  res.status(200).json(data);
-});
+	console.log("Y")
+	console.log(req.body)
+	let data = await userController.getCars(req.body)
+	res.status(200).json(data)
+})
 
 app.route("/bookcar").post(async (req, res) => {
-  let data = await userController.bookCar(req.body);
-  res.status(200).json(data);
-});
+	let data = await userController.bookCar(req.body)
+	res.status(200).json(data)
+})
 
 app.route("/updateProfile").put(async (req, res) => {
-  let data = await userController.updateProfile(req.body);
-  res.status(200).json(data);
-});
+	let data = await userController.updateProfile(req.body)
+	res.status(200).json(data)
+})
 
 app.route("/profile").post(async (req, res) => {
-  let data = await userController.profile(req.body);
-  res.status(200).json(data);
-});
+	let data = await userController.profile(req.body)
+	res.status(200).json(data)
+})
 
 app.route("/upload").post(async (req, res) => {
-  upload(req, res, async (err) => {
-    try {
-      if (err) {
-        console.log(err);
-        res.status(200).json({ errno: 400 });
-      } else {
-        const root = "/home/sandeepkumar.b/project/CRT/Backend/";
-        req.file.path = root + req.file.path;
-        const result = await userController.updateProfile({
-          email: req.body.email,
-          picture: req.file.path,
-        });
-        res.status(200).json(req.file);
-      }
-    } catch (e) {
-      res.status(200).json({ ...e, errno: 400 });
-    }
-  });
-});
+	upload(req, res, async (err) => {
+		try {
+			if (err) {
+				console.log(err)
+				res.status(200).json({ errno: 400 })
+			} else {
+				const root = "/home/sandeepkumar.b/project/CRT/Backend/"
+				req.file.path = root + req.file.path
+				const result = await userController.updateProfile({
+					email: req.body.email,
+					picture: req.file.path,
+				})
+				res.status(200).json(req.file)
+			}
+		} catch (e) {
+			res.status(200).json({ ...e, errno: 400 })
+		}
+	})
+})
 
-const user = app;
-export default user;
+const user = app
+export default user
--- Backend/tests/user.test.js
+++ Backend/tests/user.test.js
@@ -7,59 +7,58 @@ const app = new express();
 app.use("/user", user);
 
 describe("TESTS", () => {
-  beforeAll(async () => {
-    await mongoose.connect(
-      "mongodb+srv://batch6:herovired@cluster0.aqifkg2.mongodb.net/DarwinBox2"
-    );
-    console.log("TEST DB");
-  });
+	beforeAll(async () => {
+		await mongoose.connect(
+			"mongodb+srv://batch6:herovired@cluster0.aqifkg2.mongodb.net/DarwinBox2"
+		);
+		console.log("TEST DB");
+	});
 
-  test("POST /user/login", async () => {
-    const email = "sandyblaze911@gmail.com";
-    const passwd = "sandyblaze";
-    const res = await request(app).post("/user/login").send({
-      email: email,
-      passwd: passwd,
-    });
-    expect(res.statusCode).toEqual(200);
-    expect(await res.body.email).toEqual(email);
-  });
+	test("POST /user/login", async () => {
+		const email = "sandyblaze911@gmail.com";
+		const passwd = "sandyblaze";
+		const res = await request(app).post("/user/login").send({
+			email: email,
+			passwd: passwd,
+		});
+		expect(res.statusCode).toEqual(200);
+		expect(await res.body.email).toEqual(email);
+	});
 
-  //   test("POST /user/register", async () => {
-  //     const newuser = {
-  //       name: "johnswick",
-  //       email: "johns@gmail.com",
-  //       passwd: "johnwick",
-  //       phone: "1232512345",
-  //     };
-  //     const response = await request(app).post("/user/register").send(newuser);
-  //     console.log(response.body);
-  //     expect(response.statusCode).toEqual(200);
-  //     expect(response.body.name).toEqual(newuser.name);
-  //     expect(response.body.email).toEqual(newuser.email);
-  //     expect(response.body.phone).toEqual(newuser.phone);
-  //   });
+	//   test("POST /user/register", async () => {
+	//     const newuser = {
+	//       name: "johnswick",
+	//       email: "johns@gmail.com",
+	//       passwd: "johnwick",
+	//       phone: "1232512345",
+	//     };
+	//     const response = await request(app).post("/user/register").send(newuser);
+	//     console.log(response.body);
+	//     expect(response.statusCode).toEqual(200);
+	//     expect(response.body.name).toEqual(newuser.name);
+	//     expect(response.body.email).toEqual(newuser.email);
+	//     expect(response.body.phone).toEqual(newuser.phone);
+	//   });
 
-  test("POST /user/getcars", async () => {
-    const id = "641bdcb42ae3920b845bc23d";
-    const response = await request(app).post("/user/cars").send({ _id: id });
-    expect(response.statusCode).toEqual(200);
-    console.log(response.body);
-    expect(response.body.host).not.toBeNull();
-    expect(response.body.car_booked).not.toBeNull();
-  });
+	test("POST /user/getcars", async () => {
+		const id = "641bdcb42ae3920b845bc23d";
+		const response = await request(app).post("/user/cars").send({ _id: id });
+		expect(response.statusCode).toEqual(200);
+		console.log(response.body);
+		expect(response.body.host).not.toBeNull();
+		expect(response.body.car_booked).not.toBeNull();
+	});
 
-  test("POST /user/bookcar", async () => {
-    const car = {
-      car_no: "MH 12 1234",
-
-      email: "john@gmail.coam",
-      from_date: "2021-05-01",
-      from_time: "12:00",
-      to_date: "2021-05-02",
-      to_time: "12:00",
-    };
-    const response = await request(app).post("/user/bookcar").send(car);
-    expect(response.statusCode).not.toEqual(200);
-  });
+	test("POST /user/bookcar", async () => {
+		const car = {
+			car_no: "MH 12 1234",
+			email: "john@gmail.coam",
+			from_date: "2021-05-01",
+			from_time: "12:00",
+			to_date: "2021-05-02",
+			to_time: "12:00",
+		};
+		const response = await request(app).post("/user/bookcar").send(car);
+		expect(response.statusCode).toEqual(200);
+	});
 });
