681451600a46f3d9bcc86a6d9ef3bbe6fe80732a
genoos <20jayanth02@gmail.com>
2023-04-07 12:40:47 +0530

    admin gal

--- js/admin/gallery.js
+++ js/admin/gallery.js
@@ -3,18 +3,17 @@ function view(car_no) {
   location.href = "view.html";
 }
 
-function onLoad({ latitude, longitude, kms }) {
+function onLoad({ skip, limit }) {
   /**
    * Ajax code here
    */
   let data = {
-    latitude: latitude,
-    longitude: longitude,
-    kms: kms,
+    skip: skip,
+    limit: limit,
   };
   $.ajax({
     type: "POST",
-    url: "http://127.0.0.1:3000/car/nearby",
+    url: "http://127.0.0.1:3000/admin/get-some-cars",
     headers: {
       token: localStorage.getItem("token"),
     },
@@ -33,17 +32,115 @@ function onLoad({ latitude, longitude, kms }) {
   });
 }
 
-function loadCars(value) {
+function getOneCar({ car_no }) {
+  /**
+   * Ajax code here
+   */
+  let data = {
+    car_no: car_no,
+  };
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:3000/admin/get-one-car",
+    headers: {
+      token: localStorage.getItem("token"),
+    },
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    dataType: "json",
+    success: function (car) {
+      $(".one-car").empty();
+      if (car.errno != undefined) {
+        alert("No such car present");
+        $(".one-car").hide();
+        $(".card-cars").show();
+      } else {
+        $(".card-cars").hide();
+        $(".one-car").show();
+        $(".one-car").append(`
+        <div class="col-lg-3 col-md-5 col-10 py-5 mx-5">
+          <div class="card" class="col-12">
+            <img class="card-img-top" src="${car.car_picture}" alt="Card image cap" />
+            <div class="card-body">
+                <h5 class="card-title">Car No. ${car.car_no}</h5>
+                <div class="d-flex">
+                    <div class="col-12">
+                      <div class="d-flex">
+                        <p class="col-6">Company: </p>
+                        <p class="col-6">${car.company} ${car.model}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Daily cost: </p>
+                        <p class="col-6">${car.price_per_day}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Hourly cost: </p>
+                        <p class="col-6">${car.price_per_hour}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Manifactured in: </p>
+                        <p class="col-6">${car.manifactured_year}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Distance Driven: </p>
+                        <p class="col-6">${car.driven_distance}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Transmission Type: </p>
+                        <p class="col-6">${car.transmision}</p>
+                      </div>
+                      <div class="d-flex">
+                        <p class="col-6">Seater Type: </p>
+                        <p class="col-6">${car.seater_type}</p>
+                      </div>
+                    </div>
+                </div>
+                <button class="btn btn-primary">View Car</button>
+            </div>
+          </div>
+        </div>
+        `);
+      }
+    },
+  });
+}
+
+function loadCars() {
   var email = localStorage.getItem("email");
   console.log(email);
   if (email == undefined) {
     location.href = "login.html";
   }
-  $("#display-km").text(value);
-  navigator.geolocation.getCurrentPosition(function (data) {
-    data.coords.kms = +value;
-    onLoad(data.coords);
-  });
+  onLoad({ skip: 0, limit: 50 });
+}
+
+function getCars(page) {
+  if (+page < 1) {
+    page = 1;
+    $("#page-num").val(1);
+  } else {
+    $("#page-num").val(page);
+  }
+  let skip = (page - 1) * 50;
+  let limit = 50;
+  onLoad({ skip, limit });
+}
+
+function skipPage(value) {
+  let page = +$("#page-num").val() + +value;
+  getCars(page);
+}
+
+function searchCar() {
+  let car_no = $("#car-search-bar").val();
+  if (/^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$/.test(car_no)) {
+    getOneCar({ car_no });
+  } else {
+    alert("No such car present");
+    $(".one-car").empty();
+    $(".one-car").hide();
+    $(".card-cars").show();
+  }
 }
 
 let filter_attributes = new Map();
@@ -82,15 +179,12 @@ function rearrangeArray() {
 $(".fil-btn").click(function () {
   let name = $(this).attr("name");
   let [key, value] = [name, $(this).val()];
-  $(`.${key}`).css("color", "#666666");
-  $(`.${key}`).css("background-color", "#FFFFFF");
+  $(`.${key}`).attr("class", "btn col-12 fil-btn" + key);
   if (filter_attributes.has(key) && filter_attributes.get(key) == value) {
     filter_attributes.delete(key);
-    $(this).css("color", "#666666");
   } else {
     filter_attributes.set(key, value);
-    $(this).css("color", "white");
-    $(this).css("background-color", "green");
+    $(this).attr("class", "btn btn-primary col-12 fil-btn " + key);
   }
   console.log(filter_attributes);
   rearrangeArray();
@@ -156,8 +250,5 @@ function setCardCars(cars) {
 }
 
 $(document).ready(function () {
-  loadCars(15);
-  $("#sidecostinput").change(function () {
-    mainfilter();
-  });
+  loadCars();
 });
