6f9505615025d7b19ae01fb47c812cc09fe96e57
genoos <20jayanth02@gmail.com>
2023-04-10 20:32:24 +0530

    test

--- Backend/tests/coupon.test.js
+++ Backend/tests/coupon.test.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import user from "../routes/user.js";
 import ARSCoupon from "../models/coupon.js";
+import ARSUsedCoupon from "../models/usedCoupon.js";
 
 const app = new express();
 app.use("/user", user);
@@ -15,6 +16,10 @@ describe("TESTS COUPON", () => {
     console.log("TEST DB Coupon");
   });
 
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
   test("Adding new coupon", async () => {
     const coupon = {
       coupon_code: "FLAT20",
@@ -24,7 +29,23 @@ describe("TESTS COUPON", () => {
         value: 20,
       },
     };
-    const new_coupon = await new ARSCoupon(coupon);
+    const new_coupon = new ARSCoupon(coupon);
+    await new_coupon.save();
     expect(new_coupon.coupon_code).toEqual(coupon.coupon_code);
+  });
+
+  test("Find coupon", async () => {
+    const coupon_code = "ugd1423";
+    const result = await ARSCoupon.find({ coupon_code: coupon_code });
+    expect(result.coupon_code).toEqual(coupon_code);
+  });
+
+  test("Apply coupon", async () => {
+    const used_coupon = new ARSUsedCoupon({
+      user_id: "643261a4efe836a10387522f",
+      coupon_code: "qwerty",
+    });
+    const result = await used_coupon.save();
+    expect(result.coupon_code).toEqual(used_coupon.coupon_code);
   });
 });
